e800ac79a4403fb76c6b875d92e9df1eb1a669a5
nzpetter <pgolasz@gmail.com>
2018-05-13 12:45:53 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
-module.exports = {
-    mode: "development",
-    entry: ["./webpackentry.js"],
-    output: {
-        path: __dirname + "/public/js/",
-        filename: "output.js"
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.js$/],
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js']
-    }
-};
+// webpack.config.js
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .configureRuntimeEnvironment('development')
+    .cleanupOutputBeforeBuild()
+    // read main.js     -> output as web/build/app.js
+    .addEntry('appjs', './webpackentry.js')
+    // read global.scss -> output as web/build/global.css
+    .addStyleEntry('appcss', './css/sampleapp.scss')
+
+    // enable features!
+    .enableSourceMaps(!Encore.isProduction())
+    .enableSassLoader()
+    //.enableVersioning() // hashed filenames (e.g. main.abc123.js)
+    .setOutputPath('public/build/')
+    .setPublicPath('/build');
+
+module.exports = Encore.getWebpackConfig();
